15273919780a58ce11586920a8642616c0c929a1
lucasschmoeller001 <lucas.schmoeller001@umb.edu>
2026-02-15 18:57:13 -0500

    Lab 1 Java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, CS114!");
+        System.out.println("+||||||||+");
+        System.out.println("|        |");
+        System.out.println("|  0  0  |");
+        System.out.println("|  (__)  |");
+        System.out.println("|        |");
+        System.out.println("+||||||||+");
+
+
+    }
+}
